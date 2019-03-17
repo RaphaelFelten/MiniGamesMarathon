c2d7af21905a1a899a960d3906b9c03f88e1268e
Raphael Felten <raphael.felten@gmail.com>
2019-03-17 12:21:54 +0100

    Intro

--- src/games/goDownUnder/sketch.js
+++ src/games/goDownUnder/sketch.js
@@ -22,22 +22,35 @@ let engine,
     imgPlayer,
     imgObstacle;
 
+// continue to game from start screen
+document.getElementById('continue').addEventListener('click', (e) => {
+    e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+});
+
+// start game
+document.getElementById('start_game').addEventListener('click', (e) => {
+    loop();
+    e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+});
+
+// preloading some assets
 function preload() {
-    imgBackground = loadImage('src/games/goDownUnder/background-rm.png');
-    //imgPlayer = loadImage('src/games/goDownUnder/spongebob.png');
-    imgPlayer = loadImage('src/games/goDownUnder/image (2).png');
+    imgBackground = loadImage('src/games/goDownUnder/background.png');
+    imgPlayer = loadImage('src/games/goDownUnder/mrpoopybutthole.png');
     imgObstacle = loadImage('src/games/goDownUnder/obstacle.jpg');
 }
 
+// set up the game environment
 function setup() {
+    noLoop();
     let canvas = createCanvas(canvasWidth, canvasHeight);
     canvas.elt.style.left = ((window.innerWidth - canvasWidth) / 2) + 'px';
     engine = Engine.create();
     obstacleColor = [random(1, 255), random(1, 255), random(1, 255), 70];
     const boundOptions = {};
     boundOptions.isStatic = true;
-    boundLeft = Bodies.rectangle(-15, height / 2, 30, height, boundOptions);
-    boundRight = Bodies.rectangle(width + 15, height / 2, 30, height, boundOptions);
+    boundLeft = Bodies.rectangle(-50, height / 2, 100, height, boundOptions);
+    boundRight = Bodies.rectangle(width + 50, height / 2, 100, height, boundOptions);
     World.add(engine.world, [boundLeft, boundRight]);
     player = new Player();
     for (let i = 0; i < 5; i++) {
@@ -45,22 +58,15 @@ function setup() {
     }
 }
 
+// game loop
 function draw() {
     background(0);
-    image(imgBackground, 0, 0, canvasWidth, canvasHeight);
+    image(imgBackground, -100, 0);
     translate(0, -vertOff);
     Engine.update(engine);
-    Body.translate(boundLeft, {
-        x: 0,
-        y: vertSpeed
-    });
-    Body.translate(boundRight, {
-        x: 0,
-        y: vertSpeed
-    });
+    // game over
     if (player.body.position.y + player.r <= vertOff || player.body.position.y - player.r >= vertOff + canvasHeight) {
         noLoop();
-        document.write('Game Over at ' + score + ' points, reload page to try again');
     }
     player.render();
     obstacles.forEach(o => {
@@ -81,7 +87,17 @@ function draw() {
         }
         o.render();
     });
-    if (frameCount % 2 == 0) vertOff += vertSpeed;
+    if (frameCount % 2 == 0) {
+        Body.translate(boundLeft, {
+            x: 0,
+            y: vertSpeed
+        });
+        Body.translate(boundRight, {
+            x: 0,
+            y: vertSpeed
+        });
+        vertOff += vertSpeed;
+    }
     if (keyIsDown(LEFT_ARROW)) {
         player.move(-playerSpeed);
     } else if (keyIsDown(RIGHT_ARROW)) {
