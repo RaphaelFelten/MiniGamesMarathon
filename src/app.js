d7bd684eb3858847d7c248d368eff4581e768481
Raphael Felten <raphael.felten@gmail.com>
2020-05-18 07:15:20 +0200

    Converted to ES6 modules

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+import GameRunner from './GameRunner.js';
+import * as GoDownUnder from './games/GoDownUnder/GoDownUnder.js';
+
+(() => {
+    const canvas = document.getElementById('screen');
+    const gameRunner = new GameRunner(canvas);
+    gameRunner.start(GoDownUnder);
+})();
+
 
