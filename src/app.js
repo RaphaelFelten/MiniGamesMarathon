5f44b12a0abfa2c7fd9ee6f2363cd40ba2cf7ca8
Raphael Felten <raphael.felten@gmail.com>
2020-05-24 14:18:45 +0200

    Updated Solid Class
    Updated Solid Class

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import GameRunner from './GameRunner.js';
-
-const SCREEN_RATIO = 5/10;
-
-const canvas = document.getElementById('screen');
-canvas.height = window.innerHeight;
-canvas.width = window.innerHeight * SCREEN_RATIO;
-
-new GameRunner(canvas).start();
--- src/loader.js
+++
@@ -1,7 +0,0 @@
-export const loadImage = async (url) => {
-    return new Promise(resolve => {
-        const img = new Image();
-        img.src = url;
-        img.addEventListener('load', () => { resolve(img) });
-    });
-}
