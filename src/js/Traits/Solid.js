0289ec37f4bbad1326136c7e6aa5000632a47880
Raphael Felten <raphael.felten@gmail.com>
2020-05-26 17:15:45 +0200

    Updated Solid class
    Updated Solid class

--- src/js/Traits/Solid.js
+++ src/js/Traits/Solid.js
@@ -1,4 +1,4 @@
-import Obstacle from "./Obstacle.js";
+import Obstacle from './Obstacle.js';
 
 export default class Solid {
   constructor(entity) {
@@ -15,29 +15,35 @@ export default class Solid {
       return;
     }
 
-    const overlapsWith = gameContext.world.entities.filter(entity => this.overlap(entity) && entity.hasTrait(Solid));
+    const overlapsWith = gameContext.world.entities.filter(
+      (entity) => this.overlap(entity) && entity.hasTrait(Solid)
+    );
     if (overlapsWith.length < 1) {
       return;
     }
 
     if (this.entity.vel.x > 0) {
-      overlapsWith.forEach(entity => {
-        if (this.entity.sides.right > entity.sides.left &&
-            this.entity.sides.right < entity.sides.left + entity.width / 2 &&
-            this.entity.sides.left < entity.sides.left &&
-            this.entity.sides.bottom > entity.sides.top &&
-            this.entity.sides.top < entity.sides.bottom) {
+      overlapsWith.forEach((entity) => {
+        if (
+          this.entity.sides.right > entity.sides.left &&
+          this.entity.sides.right < entity.sides.left + entity.width / 2 &&
+          this.entity.sides.left < entity.sides.left &&
+          this.entity.sides.bottom > entity.sides.top &&
+          this.entity.sides.top < entity.sides.bottom
+        ) {
           this.entity.pos.x = entity.sides.left - this.entity.width;
           this.entity.vel.x *= -this.restitution;
         }
       });
     } else if (this.entity.vel.x < 0) {
-      overlapsWith.forEach(entity => {
-        if (this.entity.sides.left < entity.sides.right &&
-            this.entity.sides.left > entity.sides.right - entity.width / 2 &&
-            this.entity.sides.right > entity.sides.right &&
-            this.entity.sides.bottom > entity.sides.top &&
-            this.entity.sides.top < entity.sides.bottom) {
+      overlapsWith.forEach((entity) => {
+        if (
+          this.entity.sides.left < entity.sides.right &&
+          this.entity.sides.left > entity.sides.right - entity.width / 2 &&
+          this.entity.sides.right > entity.sides.right &&
+          this.entity.sides.bottom > entity.sides.top &&
+          this.entity.sides.top < entity.sides.bottom
+        ) {
           this.entity.pos.x = entity.sides.right;
           this.entity.vel.x *= -this.restitution;
         }
@@ -45,37 +51,43 @@ export default class Solid {
     }
 
     if (this.entity.vel.y > 0) {
-      overlapsWith.forEach(entity => {
-        if (this.entity.sides.bottom > entity.sides.top &&
+      overlapsWith.forEach((entity) => {
+        if (
+          (this.entity.sides.bottom > entity.sides.top &&
             this.entity.sides.bottom < entity.sides.top + entity.height / 2 &&
             this.entity.sides.top < entity.sides.top &&
             this.entity.sides.right > entity.sides.left &&
-            this.entity.sides.left < entity.sides.right) {
+            this.entity.sides.left < entity.sides.right) ||
+          (this.entity.vel.y > entity.height / 2 &&
+            this.entity.top > entity.top)
+        ) {
           this.entity.pos.y = entity.sides.top - this.entity.height;
           this.entity.vel.y *= -this.restitution;
         }
       });
     } else if (this.entity.vel.y < 0) {
-      overlapsWith.forEach(entity => {
-        if (this.entity.sides.top < entity.sides.bottom &&
-            this.entity.sides.top > entity.sides.bottom - entity.height / 2 &&
-            this.entity.sides.bottom > entity.sides.bottom &&
-            this.entity.sides.right > entity.sides.left &&
-            this.entity.sides.left < entity.sides.right) {
+      overlapsWith.forEach((entity) => {
+        if (
+          this.entity.sides.top < entity.sides.bottom &&
+          this.entity.sides.top > entity.sides.bottom - entity.height / 2 &&
+          this.entity.sides.bottom > entity.sides.bottom &&
+          this.entity.sides.right > entity.sides.left &&
+          this.entity.sides.left < entity.sides.right
+        ) {
           this.entity.pos.y = entity.sides.bottom;
           this.entity.vel.y *= -this.restitution;
         }
       });
     }
-
   }
 
   overlap(entity) {
-    return this.entity !== entity &&
-           this.entity.sides.bottom > entity.sides.top &&
-           this.entity.sides.top < entity.sides.bottom &&
-           this.entity.sides.right > entity.sides.left &&
-           this.entity.sides.left < entity.sides.right;
+    return (
+      this.entity !== entity &&
+      this.entity.sides.bottom > entity.sides.top &&
+      this.entity.sides.top < entity.sides.bottom &&
+      this.entity.sides.right > entity.sides.left &&
+      this.entity.sides.left < entity.sides.right
+    );
   }
-
 }
